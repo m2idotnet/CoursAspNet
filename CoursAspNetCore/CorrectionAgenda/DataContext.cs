c05208978be86567680b5204013dd3755274e24d
m2i <m2idotnet@gmail.com>
2019-06-13 15:38:32 +0200

    AspnetCore MVC Correction Agenda

---
+++ CoursAspNetCore/CorrectionAgenda/DataContext.cs
@@ -0,0 +1,57 @@
+﻿using CorrectionAgenda.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorrectionAgenda
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+            //RelationalDatabaseCreator databaseCreator = (RelationalDatabaseCreator)Database.GetService<IRelationalDatabaseCreator>();
+            //databaseCreator.CreateTables();
+            
+        }
+
+        private  void CreateTable()
+        {
+           // SpecialDataBase databaseCreator = (SpecialDataBase)Database.GetService<SqlServerDatabaseCreator>();
+           //if(databaseCreator.testTable())
+           // {
+           //     databaseCreator.CreateTables();
+           // }
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\DataBaseEntityFrameWork;Integrated Security=True");
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+
+        public DbSet<Email> Emails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+
+    public class SpecialDataBase : SqlServerDatabaseCreator
+    {
+        public SpecialDataBase( RelationalDatabaseCreatorDependencies dependencies, ISqlServerConnection connection,  IRawSqlCommandBuilder rawSqlCommandBuilder) : base(dependencies, connection, rawSqlCommandBuilder)
+        {
+        }
+
+        public bool testTable()
+        {
+            return HasTables();
+        }
+    }
+}
