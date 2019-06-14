bd8a46c20e205c2f74fc104eec1f43f323f26f9f
m2i <m2idotnet@gmail.com>
2019-06-14 10:18:29 +0200

    Cours AspNetCore Mvc Form

--- CoursAspNetCore/CoursAspNetCore/Models/Personne.cs
+++ CoursAspNetCore/CoursAspNetCore/Models/Personne.cs
@@ -11,12 +11,17 @@ namespace CoursAspNetCore.Models
         private int id;
         private string nom;
         private string prenom;
+        private string tel;
 
         public int Id { get => id; set => id = value; }
-        [Display (Name = "Nom du client"), Required, MaxLength(50)]
+        [Display (Name = "Nom du client"), Required(ErrorMessage ="Erreur"), MaxLength(50, ErrorMessage ="Erreur taille")]
         public string Nom { get => nom; set => nom = value; }
         [Display(Name = "Prénom du client")]
         public string Prenom { get => prenom; set => prenom = value; }
+
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^0[1-9]{1}([\s*-\.]?[0-9]{2}){4}", ErrorMessage ="format incorrect")]
+        public string Tel { get => tel; set => tel = value; }
 
         public static List<Personne> liste = new List<Personne>()
         {
