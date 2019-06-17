de47009ba69db73e1d3fa3d80acfce596c3d893f
m2i <m2idotnet@gmail.com>
2019-06-17 15:00:13 +0200

    Correction Session Panier

---
+++ CoursAspNetCore/PanierAspNetCore/Controllers/ProduitController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PanierAspNetCore.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PanierAspNetCore.Controllers
+{
+    public class ProduitController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View(DataBaseContext.Instance.Produits.ToList());
+        }
+    }
+}
---
+++ CoursAspNetCore/PanierAspNetCore/Models/Produit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PanierAspNetCore.Models
+{
+    public class Produit
+    {
+        private int id;
+        private string titre;
+        private string image;
+        private decimal prix;
+
+        public int Id { get => id; set => id = value; }
+        public string Titre { get => titre; set => titre = value; }
+        public string Image { get => image; set => image = value; }
+        public decimal Prix { get => prix; set => prix = value; }
+
+        
+    }
+}
