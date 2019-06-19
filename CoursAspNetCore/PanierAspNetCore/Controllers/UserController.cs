4fcb42c404f50a5efcd8b208ee3879639b963681
AlexDr <localhost@localhost.com>
2019-06-19 09:46:32 +0200

    push local

--- CoursAspNetCore/PanierAspNetCore/Controllers/UserController.cs
+++ CoursAspNetCore/PanierAspNetCore/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace PanierAspNetCore.Controllers
@@ -31,7 +32,7 @@ namespace PanierAspNetCore.Controllers
             MD5 crypto = MD5.Create();
             string hash = serviceLogin.GetMd5Hash(crypto, password);
             User u = DataBaseContext.Instance.Users.FirstOrDefault((x) => x.UserName == username && x.Password == hash);
-            if(u != null)
+            if (u != null)
             {
                 u.Token = serviceLogin.GetMd5Hash(crypto, Guid.NewGuid().ToString());
                 DataBaseContext.Instance.SaveChanges();
@@ -57,5 +58,36 @@ namespace PanierAspNetCore.Controllers
             serviceLogin.LogOut(Response);
             return RedirectToAction("Index", "Produit");
         }
+
+        //[HttpGet]
+        //public IActionResult SaveInLocal(string u, string p_1, string p_2)
+        //{
+        //    CookieOptions o = new CookieOptions
+        //    {
+        //        Expires = DateTime.Now.AddHours(1)
+        //    };
+
+        //    SHA512 shaM = SHA512.Create();
+        //    byte[] data1 = Encoding.UTF8.GetBytes(p_1);
+        //    byte[] data2 = Encoding.UTF8.GetBytes(p_2);
+        //    byte[] hash1 = shaM.ComputeHash(data1);
+        //    byte[] hash2 = shaM.ComputeHash(data2);
+
+        //    if (p_1.Length != p_2.Length)
+        //    {
+        //        if (hash1.SequenceEqual(hash2))
+
+        //        {
+        //            return false;
+        //        }
+        //    }
+
+        //    Response.Cookies.Append("usernameInscription", u,o);
+        //    Response.Cookies.Append("password1", p_1,o);
+        //    Response.Cookies.Append("password2", p_2,o);
+
+        //    return RedirectToPage();
+        //}
+
     }
 }
