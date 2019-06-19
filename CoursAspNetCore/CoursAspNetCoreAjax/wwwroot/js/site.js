2b77c20dc51d733b3dc28edd58de3fe21d6ad182
m2i <m2idotnet@gmail.com>
2019-06-19 12:13:52 +0200

    Cours Ajax AspNETCore partie 1

---
+++ CoursAspNetCore/CoursAspNetCoreAjax/wwwroot/js/site.js
@@ -0,0 +1,30 @@
+﻿//Effectuer une requete ajax en utilisant l'objet jquery dés le chargement de la page
+//$.ajax({
+//    url: '/Home/TestAjax',
+//    type: 'GET',
+//    dataType: 'html',
+//    success: (response) => {
+//        alert(response);
+//    }
+//})
+
+//Effectuer une requete ajax en cliquant sur un lien
+$('.reqAjax').on('click', function (e) {
+    e.preventDefault();
+    $("#resultAjax").html("en cours de chargement");
+    $.ajax({
+        //Selection du href du lien sur le quel on clique
+        url: $(this).attr('href'),
+        type: 'GET',
+        dataType: 'html',
+        //data : "toto=",
+        success: (responseData) => {
+            
+            $("#resultAjax").html(responseData);
+        },
+        error: (err) => {
+            console.dir(err);
+            alert("Erreur serveur : " + err.statusText)
+        }
+    })
+});
