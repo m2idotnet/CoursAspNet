900f6fde387f62c137fb8bedcb110aa7e416b46b
m2i <m2idotnet@gmail.com>
2019-06-19 16:51:20 +0200

    correction formulaire ajax

---
+++ CoursAspNetCore/CorrectionAjaxFormulaire/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorrectionAjaxFormulaire.Models
+{
+    public class User
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private string tel;
+        private string email;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Tel { get => tel; set => tel = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
---
+++ CoursAspNetCore/CorrectionAjaxFormulaire/wwwroot/js/site.js
@@ -0,0 +1,34 @@
+﻿// Write your JavaScript code.
+$('.champ').on('keyup', function (e) {
+    $.ajax({
+        url: "/Home/TestChamp",
+        type: "GET",
+        dataType: "json",
+        data: "nomChamp=" + $(this).attr('id') + "&valueChamp=" + $(this).val(),
+        success: (response) => {
+            if (response.error) {
+                $('span[data-valmsg-for=' + $(this).attr('id') + ']').html(response.message);
+            }
+            else {
+                $('span[data-valmsg-for=' + $(this).attr('id') + ']').html('');
+            }
+        }
+    })
+})
+
+$('#addUserForm').on('submit', function (e)  {
+    e.preventDefault();
+    $('.btnValider').html("En cours d'inscription");
+    $.ajax({
+        url: $(this).attr('action'),
+        type: $(this).attr('method'),
+        dataType: "json",
+        data: $(this).serialize(),
+        success: (response) => {
+            if (!response.error) {
+                $('.btnValider').html(response.message);
+                $('.btnValider').attr('disabled', true);
+            }
+        }
+    })
+})
