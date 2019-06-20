fd6789979eb9813a88c8647ae8112687ba11a2f2
m2i <m2idotnet@gmail.com>
2019-06-20 16:10:37 +0200

    Cours ASPNET api Rest

---
+++ CoursAspNetCore/CoursApiRest/Controllers/PersonneController.cs
@@ -0,0 +1,68 @@
+﻿using CoursApiRest.Models;
+using CoursApiRest.Tools;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursApiRest.Controllers
+{
+    [Route("api/[controller]")]
+    public class PersonneController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Personne> Get()
+        {
+            return DataBaseContext.Instance.Personnes.ToList();
+        }
+
+
+        [HttpGet("{id}")]
+        public Personne Get(int id)
+        {
+            return DataBaseContext.Instance.Personnes.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public bool Post([FromBody]Personne p)
+        {
+            p.Add();
+            return p.Id > 0;
+        }
+
+        [HttpPut("{id}")]
+        public bool Put(int id, [FromBody]Personne p)
+        {
+            Personne personne = DataBaseContext.Instance.Personnes.FirstOrDefault(x => x.Id == id);
+            if(personne != null)
+            {
+                personne.Nom = (p.Nom != null) ? p.Nom : personne.Nom;
+                personne.Prenom = (p.Prenom != null) ? p.Prenom : personne.Prenom;
+                personne.Tel = (p.Tel != null) ? p.Tel : personne.Tel;
+                DataBaseContext.Instance.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            Personne personne = DataBaseContext.Instance.Personnes.FirstOrDefault(x => x.Id == id);
+            if(personne != null)
+            {
+                DataBaseContext.Instance.Personnes.Remove(personne);
+                DataBaseContext.Instance.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ CoursAspNetCore/CoursApiRest/Models/Personne.cs
@@ -0,0 +1,27 @@
+﻿using CoursApiRest.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursApiRest.Models
+{
+    public class Personne
+    {
+        private int id;
+        private string nom;
+        private string prenom;
+        private string tel;
+
+        public int Id { get => id; set => id = value; }
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public string Tel { get => tel; set => tel = value; }
+
+        public void Add()
+        {
+            DataBaseContext.Instance.Add(this);
+            DataBaseContext.Instance.SaveChanges();
+        }
+    }
+}
